c7e44f84443eac037daf64aa0a0b7dc42b54aad6
MF <notsure@compuserv.com>
2017-11-13 21:43:45 -0600

    Initial design of final project

---
+++ app/src/main/java/com/mark/androidfinal/Book.java
@@ -0,0 +1,74 @@
+package com.mark.androidfinal;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * This Class outlines a Book object and the attributes it contains.
+ */
+
+public class Book {
+    private String book_name;
+    private String reader;
+    private Date start_date;
+    private Date end_date;
+    private int total_pages;
+    private int pages_read;
+    private ArrayList<Float> hours_spent_per_week;
+    private boolean completed;
+
+    public Book(String book_name, String reader, int total_pages) {
+        this.book_name = book_name;
+        this.reader = reader;
+        this.start_date = new Date();
+        this.total_pages = total_pages;
+        this.completed = false;
+    }
+
+// Getters and Setters.
+    public String getBook_name() {
+        return book_name;
+    }
+
+    public String getReader() {
+        return reader;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date() {
+        this.end_date = new Date();
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public int getPages_read() {
+        return pages_read;
+    }
+
+    // This Setter might want to be merged with add_hours_for_week method/setter.
+    public void setPages_read(int pages_read) {
+        this.pages_read = pages_read;
+    }
+
+    public ArrayList<Float> getHours_spent_per_week() {
+        return hours_spent_per_week;
+    }
+
+    public void add_hours_for_week(float hours) {
+        // TODO determine which week it currently is
+        // TODO add hours to that week in arraylist
+    }
+}
---
+++ app/src/main/java/com/mark/androidfinal/MainActivity.java
@@ -0,0 +1,57 @@
+package com.mark.androidfinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+            Fragment fragment = new Fragment();
+            FragmentManager fm = getSupportFragmentManager();
+
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:      // Maybe view all books in database?
+//                    mTextMessage.setText(R.string.title_home);
+//                    return true;
+                case R.id.navigation_dashboard: // Maybe add new book?
+                    fragment = new NewBookFragment();
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+                case R.id.navigation_notifications: // Maybe update existing book?
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
+            }
+            if (fragment != null) {
+                fm.beginTransaction().replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
