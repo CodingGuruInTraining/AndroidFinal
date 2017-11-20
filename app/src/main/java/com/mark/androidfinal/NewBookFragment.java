38ffb980a267b7beb0730f655d39d49f1ce4f954
MF <notsure@compuserv.com>
2017-11-19 18:04:35 -0600

    tried getting nav bar to stay on bottom started capturing inputs for new book fragment

--- app/src/main/java/com/mark/androidfinal/NewBookFragment.java
+++ app/src/main/java/com/mark/androidfinal/NewBookFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Fragment Class
@@ -14,6 +16,22 @@ public class NewBookFragment extends Fragment {
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new_book, container, false);
+        View view = inflater.inflate(R.layout.fragment_new_book, container, false);
+
+        final EditText nameEditText = (EditText) view.findViewById(R.id.new_book_name);
+        final EditText readerEditText = (EditText) view.findViewById(R.id.new_book_reader);
+        final EditText pagesEditText = (EditText) view.findViewById(R.id.new_book_pages);
+        Button submitButton = (Button) view.findViewById(R.id.new_book_submit_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEditText.getText().toString();
+                String reader = readerEditText.getText().toString();
+                int pages = Integer.parseInt(pagesEditText.getText().toString());
+            }
+        });
+
+        return view;
     }
 }
